80c1fcaa5135d94515bc7bac98f6a2cd4162b433
Konstantin Lukas <konstantin_lukas@web.de>
2024-03-20 22:02:03 +0100

    add day5 2023 part 2

--- 2023/day5/day5.rs
+++ 2023/day5/day5.rs
@@ -1,9 +1,20 @@
 use std::fs;
-use std::collections::HashMap;
+use std::cmp::{max, min};
+
+
+fn map_seed(seed: i64, map: &Vec<(i64, i64, i64)>) -> i64 {
+    for value in map {
+        if seed >= value.0 && seed < value.0 + value.2 {
+            return seed + value.1 - value.0;
+        }
+    }
+    return seed;
+}
+
 fn main() {
     let data = fs::read_to_string("day5.data").expect("Unable to read file");
     let data = data.split("\n\n").map(String::from);
-    let mut maps: [HashMap<i64, (i64, i64)>; 7] = Default::default();
+    let mut maps: [Vec<(i64, i64, i64)>; 7] = Default::default();
     let mut seeds: Vec<i64> = vec![];
     let mut locations: Vec<i64> = vec![];
     for (i, datum) in data.enumerate() {
@@ -25,27 +36,62 @@ fn main() {
                     let dest = data[0].parse::<i64>().unwrap();
                     let src = data[1].parse::<i64>().unwrap();
                     let len = data[2].parse::<i64>().unwrap();
-                    maps[i - 1].insert(src, (dest, len));
+                    maps[i - 1].push((src, dest, len));
                 }
             }
         }
     }
-    for seed in seeds {
-        let mut seed = seed;
-        'maps: for map in &maps {
-            for value in map {
-                let src = *(value.0);
-                let dest = value.1.0;
-                let len = value.1.1;
-                if seed >= src && seed < src + len {
-                    seed += dest - src;
-                    continue 'maps;
-                }
-            }
+
+    // PART 1
+    for seed in &seeds {
+        let mut seed = *seed;
+        for map in &maps {
+            seed = map_seed(seed, map);
         }
         locations.push(seed);
     }
-    let min = locations.iter().min().unwrap();
-    println!("{}", min);
+    let minimum = locations.iter().min().unwrap();
+    println!("{}", minimum);
+
+    // PART 2
+    let mut seeds: Vec<(i64, i64)> = seeds.chunks(2).map(|chunk| (chunk[0], chunk[0] + chunk[1])).collect();
+    for map in &maps {
+        let mut overlaps: Vec<(i64, i64)> = vec![];
+        while seeds.len() > 0 {
+            let (seed_start, seed_end) = seeds.pop().expect("Couldn't pop");
+            let mut found_overlap = false;
+            for rule in map {
+                let rule_start = rule.0;
+                let rule_end = rule.0 + rule.2;
+                let rule_dest = rule.1;
+                let overlap_start = max(seed_start, rule_start);
+                let overlap_end = min(seed_end, rule_end);
+                if overlap_start < overlap_end {
+                    overlaps.push(
+                        (overlap_start + rule_dest - rule_start, overlap_end + rule_dest - rule_start)
+                    );
+                    if overlap_start > seed_start {
+                        seeds.push((seed_start, overlap_start));
+                    }
+                    if overlap_end < seed_end {
+                        seeds.push((overlap_end, seed_end));
+                    }
+                    found_overlap = true;
+                    break;
+                }
+            }
+            if !found_overlap {
+                overlaps.push((seed_start, seed_end));
+            }
+        }
+        seeds = overlaps;
+    }
+
+    if let Some(min_tuple) = seeds.iter().min_by_key(|&(first, _)| first) {
+        println!("{}",min_tuple.0);
+    }
+
+
+    //println!("{}", seed);
 
 }
