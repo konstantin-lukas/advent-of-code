fa70736af5bf1fabee1aac15fb6187dd5af76fd5
Konstantin Lukas <konstantin_lukas@web.de>
2024-04-01 22:50:10 +0200

    port day1 from python to rust

--- 2023-rust/src/main.rs
+++ 2023-rust/src/main.rs
@@ -2,6 +2,9 @@ pub mod utils;
 pub mod solutions;
 
 fn main() {
+    let (part1, part2) = solutions::day1::run(false);
+    println!("DAY 1, PART 1: {part1}");
+    println!("DAY 1, PART 2: {part2}");
     let (part1, part2) = solutions::day2::run(false);
     println!("DAY 2, PART 1: {part1}");
     println!("DAY 2, PART 2: {part2}");
@@ -19,6 +22,12 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use crate::solutions;
+    #[test]
+    fn day1() {
+        let (part1, part2) = solutions::day1::run(true);
+        assert_eq!(part1, 297);
+        assert_eq!(part2, 281);
+    }
     #[test]
     fn day2() {
         let (part1, part2) = solutions::day2::run(true);
