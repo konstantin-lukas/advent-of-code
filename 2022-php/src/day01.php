aa6928957b0ac7e77b42e4d9a293e6607e794ad9
konstantin <konstantin_lukas@web.de>
2024-11-17 12:56:31 +0100

    solve day 1 part 1

--- 2022-php/src/day01.php
+++ 2022-php/src/day01.php
@@ -1,9 +1,19 @@
 <?php
 require_once "day.php";
 class Day01 extends Day {
+
+    public ?int $testResultPart1 = 24000;
+
     public function part1(bool $test): int {
         $data = $test ? $this->testData : $this->data;
-        return 0;
+        $elves = explode(PHP_EOL.PHP_EOL, $data);
+        $maxCalories = array_map(function($elf) {
+            $individualCalories = array_map(function ($calorie) {
+                return intval($calorie);
+            },explode(PHP_EOL, $elf));
+            return array_sum($individualCalories);
+        },$elves);
+        return max($maxCalories);
     }
 
     public function part2(bool $test): int {
