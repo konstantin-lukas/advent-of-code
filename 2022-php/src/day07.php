e8999606b53bc549f170c69458274d6b8a4bb15a
konstantin <konstantin_lukas@web.de>
2024-11-24 14:26:11 +0100

    solve day 7 part 1

--- 2022-php/src/day07.php
+++ 2022-php/src/day07.php
@@ -1,10 +1,75 @@
 <?php
 require_once "day.php";
 
+class Dir {
+    public array $children = [];
+    public function __construct(public string $name, public ?Dir &$parent = null) {}
+}
+
+class File {
+    public function __construct(public int $size, public string $name, public Dir &$parent) {}
+}
+
+class Command {
+    public string $name;
+    public string $arg;
+}
+
 class Day07 extends Day {
 
+    public mixed $testResultPart1 = 95437;
+
+    private function parse(bool $test): Dir
+    {
+        $data = $test ? $this->testData : $this->data;
+        $root = new Dir("/");
+        $wd = &$root;
+        $lines = explode(PHP_EOL, $data);
+        foreach ($lines as $line) {
+            if (str_starts_with($line, "$")) {
+                if ($line[2] === "c") {
+                    $name = substr($line, 5);
+                    if ($name === "/") {
+                        $wd = &$root;
+                        continue;
+                    }
+                    if ($name === "..") {
+                        $wd = &$wd->parent;
+                    } else {
+                        for ($i = 0; $i < count($wd->children); $i++) {
+                            if ($wd->children[$i] instanceof Dir && $wd->children[$i]->name === $name) {
+                                $wd = &$wd->children[$i];
+                                break;
+                            }
+                        }
+                    }
+                }
+            } else {
+                if (str_starts_with($line, "dir")) {
+                    $wd->children[] = new Dir(substr($line, 4), $wd);
+                } else {
+                    [$size, $name] = explode(" ", $line);
+                    $wd->children[] = new File($size, $name, $wd);
+                }
+            }
+        }
+        return $root;
+    }
+
+    private function traverse(Dir | File $node, array& $dirSizes): int {
+        if ($node instanceof File) return $node->size;
+        $result = 0;
+        foreach ($node->children as $child) {
+            $result += $this->traverse($child, $dirSizes);
+        }
+        $dirSizes[] = $result;
+        return $result;
+    }
     public function part1(bool $test): int {
-        return 0;
+        $root = $this->parse($test);
+        $dirSizes = [];
+        $this->traverse($root, $dirSizes);
+        return array_sum(array_filter($dirSizes, fn ($x) => $x <= 100000));
     }
 
     public function part2(bool $test): int {
--- README.md
+++ README.md
@@ -15,9 +15,9 @@ were executed on an AMD Ryzen 7 7700X.
 | 2 | 333.047μs <!-- 333047 --> | 295.457μs <!-- 295457 --> | [day02.php](https://github.com/konstantin-lukas/advent-of-code/blob/master/2022-php/src/day02.php) |
 | 3 | 613.695μs <!-- 613695 --> | 584.005μs <!-- 584005 --> | [day03.php](https://github.com/konstantin-lukas/advent-of-code/blob/master/2022-php/src/day03.php) |
 | 4 | 459.226μs <!-- 459226 --> | 436.467μs <!-- 436467 --> | [day04.php](https://github.com/konstantin-lukas/advent-of-code/blob/master/2022-php/src/day04.php) |
-| 5 | 184.058μs <!-- 184058 --> | 162.639μs <!-- 162639 --> | [day05.php](https://github.com/konstantin-lukas/advent-of-code/blob/master/2022-php/src/day05.php) |
-| 6 | 191.308μs <!-- 191308 --> | 940.232μs <!-- 940232 --> | [day06.php](https://github.com/konstantin-lukas/advent-of-code/blob/master/2022-php/src/day06.php) |
-| 7 | N/A <!-- 9223372036854775807 --> | N/A <!-- 9223372036854775807 --> | [day07.php](https://github.com/konstantin-lukas/advent-of-code/blob/master/2022-php/src/day07.php) |
+| 5 | 184.058μs <!-- 184058 --> | 162.338μs <!-- 162338 --> | [day05.php](https://github.com/konstantin-lukas/advent-of-code/blob/master/2022-php/src/day05.php) |
+| 6 | 189.038μs <!-- 189038 --> | 940.232μs <!-- 940232 --> | [day06.php](https://github.com/konstantin-lukas/advent-of-code/blob/master/2022-php/src/day06.php) |
+| 7 | 148.379μs <!-- 148379 --> | N/A <!-- 9223372036854775807 --> | [day07.php](https://github.com/konstantin-lukas/advent-of-code/blob/master/2022-php/src/day07.php) |
 | 8 | N/A <!-- 9223372036854775807 --> | N/A <!-- 9223372036854775807 --> | [day08.php](https://github.com/konstantin-lukas/advent-of-code/blob/master/2022-php/src/day08.php) |
 | 9 | N/A <!-- 9223372036854775807 --> | N/A <!-- 9223372036854775807 --> | [day09.php](https://github.com/konstantin-lukas/advent-of-code/blob/master/2022-php/src/day09.php) |
 | 10 | N/A <!-- 9223372036854775807 --> | N/A <!-- 9223372036854775807 --> | [day10.php](https://github.com/konstantin-lukas/advent-of-code/blob/master/2022-php/src/day10.php) |
